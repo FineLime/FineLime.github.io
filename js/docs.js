361d2ff752a95396954b9f0800ca20f65fb9c00f
FineLime <42850488+FineLime@users.noreply.github.com>
2020-07-25 03:42:18 +0100

    Update docs.js

--- js/docs.js
+++ js/docs.js
@@ -1,8 +1,7 @@
 //Modules to appear when you click "+"
 var modules = [";purge {type} [amount] {text} - Delete x amount of messages based on the type given (contains, match, images), if any is given<br><br>;purgeban {type} [amount] {text} - Same as above, but bans the user as well as deleting the message, allowing to easily ban raiders<br><br>;ban [user] {reason} - Bans the user specified<br><br>;kick [user] {reason} - Kicks the user specified",
 	";createreactionrole [messageID] [role] [emoji] - Creates a new reaction role, must be used in the same channel as the message<br><br>;deletereactionrole [messageID] [emoji] - Deletes the reaction role on a message, reactions will not be automatically removed<br><br>;newemoji [name] **file** - Creates a new emoji with the name given and file sent<br><br>;listroles - List all roles in the server along with their ID<br><br>;say [message] - Makes the bot send a message<br><br>;ping - Responds with the bots ping", 
-	";8ball [question] - Responds with a random answer to the question<br><br>;coinflip - Flips a coin<br><br>;diceroll {amount} - Rolls a die, max amount will be 6 if an amount of sides is not specified<br><br>;slots - Slots machine",
-	";coins [user - default you] - Checks how many coins the user has!"
+	";8ball [question] - Responds with a random answer to the question<br><br>;coinflip - Flips a coin<br><br>;diceroll {amount} - Rolls a die, max amount will be 6 if an amount of sides is not specified<br><br>;slots - Slots machine<br><br><br>;launch {Launch Provider} - Gets the next launch, with a link if found. You can find the next launch by launch provider as well.",
 	
 ];
 
